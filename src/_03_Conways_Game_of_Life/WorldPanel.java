29901bc7d785e849ed93b778bc32fa644275f959
League Student <league@iMac-24.attlocal.net>
2019-09-09 18:27:20 -0700

    ConwaysGameOfLife(IP)

--- src/_03_Conways_Game_of_Life/WorldPanel.java
+++ src/_03_Conways_Game_of_Life/WorldPanel.java
@@ -1,4 +1,5 @@
 package _03_Conways_Game_of_Life;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -15,85 +16,217 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	private static final long serialVersionUID = 1L;
 	private int cellsPerRow;
 	private int cellSize;
-	
-	private Timer timer;
-	
-	//1. Create a 2D array of Cells. Do not initialize it.
 
-	
-	
+	private Timer timer;
+
+	// 1. Create a 2D array of Cells. Do not initialize it.
+
+	Cell[][] twoDArray;
+
 	public WorldPanel(int w, int h, int cpr) {
 		setPreferredSize(new Dimension(w, h));
 		addMouseListener(this);
 		timer = new Timer(500, this);
 		this.cellsPerRow = cpr;
-	
-		//2. Calculate the cell size.
-		
-		//3. Initialize the cell array to the appropriate size.
-		
-		//3. Iterate through the array and initialize each cell.
-		//   Don't forget to consider the cell's dimensions when 
-		//   passing in the location.
-		
+
+		// 2. Calculate the cell size.
+
+		cellSize = w / cpr;
+
+		// 3. Initialize the cell array to the appropriate size.
+
+		twoDArray = new Cell[cpr][cpr];
+
+		// 3. Iterate through the array and initialize each cell.
+		// Don't forget to consider the cell's dimensions when
+		// passing in the location.
+
+		for (int i = 0; i < twoDArray.length; i++) {
+
+			for (int j = 0; j < twoDArray[i].length; j++) {
+
+				twoDArray[i][j] = new Cell(cellSize * i, cellSize * j, cellSize);
+
+			}
+
+		}
+
 	}
-	
+
 	public void randomizeCells() {
-		//4. Iterate through each cell and randomly set each
-		//   cell's isAlive memeber to true of false
-		
+		// 4. Iterate through each cell and randomly set each
+		// cell's isAlive memeber to true of false
+
+		for (int i = 0; i < twoDArray.length; i++) {
+			for (int j = 0; j < twoDArray[i].length; j++) {
+
+				int random = new Random().nextInt(2);
+
+				if (random == 0) {
+
+					twoDArray[i][j].isAlive = true;
+
+				}
+
+				else {
+
+					twoDArray[i][j].isAlive = false;
+
+				}
+			}
+
+		}
 		repaint();
 	}
-	
+
 	public void clearCells() {
-		//5. Iterate through the cells and set them all to dead.
-		
+		// 5. Iterate through the cells and set them all to dead.
+
+		for (int i = 0; i < twoDArray.length; i++) {
+
+			for (int j = 0; j < twoDArray[i].length; j++) {
+
+				twoDArray[i][j].isAlive = false;
+
+			}
+
+		}
+
 		repaint();
 	}
-	
+
 	public void startAnimation() {
 		timer.start();
 	}
-	
+
 	public void stopAnimation() {
 		timer.stop();
 	}
-	
+
 	public void setAnimationDelay(int sp) {
 		timer.setDelay(sp);
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
-		//6. Iterate through the cells and draw them all
-		
-		
-		
+		// 6. Iterate through the cells and draw them all
+
+		for (int i = 0; i < twoDArray.length; i++) {
+
+			for (int j = 0; j < twoDArray[i].length; j++) {
+
+				twoDArray[i][j].draw(g);
+
+			}
+
+		}
+
 		// draws grid
 		g.setColor(Color.BLACK);
 		g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
 	}
-	
-	//advances world one step
+
+	// advances world one step
 	public void step() {
-		//7. iterate through cells and fill in the livingNeighbors array
+		// 7. iterate through cells and fill in the livingNeighbors array
 		// . using the getLivingNeighbors method.
 		int[][] livingNeighbors = new int[cellsPerRow][cellsPerRow];
-		
-		//8. check if each cell should live or die
-	
-		
-		
-		
+
+		for (int i = 0; i < twoDArray.length; i++) {
+			for (int j = 0; j < twoDArray[i].length; j++) {
+
+				livingNeighbors[i][j] = getLivingNeighbors(i, j);
+
+			}
+		}
+
+		// 8. check if each cell should live or die
+
+		for (int i = 0; i < twoDArray.length; i++) {
+			for (int j = 0; j < twoDArray[i].length; j++) {
+
+				twoDArray[i][j].liveOrDie(livingNeighbors.length);
+
+			}
+		}
+
 		repaint();
+
 	}
-	
-	//9. Complete the method.
-	//   It returns an int of 8 or less based on how many
-	//   living neighbors there are of the 
-	//   cell identified by x and y
-	public int getLivingNeighbors(int x, int y){
-		return 0;
+
+	// 9. Complete the method.
+	// It returns an int of 8 or less based on how many
+	// living neighbors there are of the
+	// cell identified by x and y
+	public int getLivingNeighbors(int x, int y) {
+
+		int livingNeighbors = 0;
+
+		if (x > 0) {
+
+			if (twoDArray[x - 1][y].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (x > 0 && y > 0) {
+
+			if (twoDArray[x - 1][y - 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (y > 0) {
+
+			if (twoDArray[x][y - 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (x > 0 && y > 0) {
+
+			if (twoDArray[x + 1][y - 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (x > 0) {
+
+			if (twoDArray[x + 1][y - 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (x > 0 && y > 0) {
+
+			if (twoDArray[x - 1][y + 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (x > 0) {
+
+			if (twoDArray[x][y + 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		if (x > 0 && y > 0) {
+
+			if (twoDArray[x + 1][y + 1].isAlive)
+
+				livingNeighbors++;
+
+		}
+
+		return livingNeighbors;
 	}
 
 	@Override
@@ -104,35 +237,32 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		//10. Use e.getX() and e.getY() to determine
-		//    which cell is clicked. Then toggle
-		//    the isAlive variable for that cell.
-		
-		
-		
-		
+		// 10. Use e.getX() and e.getY() to determine
+		// which cell is clicked. Then toggle
+		// the isAlive variable for that cell.
+
 		repaint();
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		step();		
+		step();
 	}
 }
